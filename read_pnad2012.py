1dc0a4e6215a73e62dae09ef929ea9cfc6a8af46
Neylson Crepalde <neylsoncrepalde@gmail.com>
2018-04-25 20:56:51 -0300

    some initial analysis

--- read_pnad2012.py
+++ read_pnad2012.py
@@ -10,3 +10,17 @@ import pandas as pd
 
 pnad = pd.read_csv('https://github.com/neylsoncrepalde/introducao_ao_python/blob/master/pes_2012.csv?raw=true')
 pnad.head()
+
+pnad.describe()
+pnad.head()
+pnad['V0404'].value_counts()
+
+
+tab = pd.crosstab(index=pnad['V0404'],  # Make a crosstab
+                              columns="count")      # Name the count column
+
+print('Distribuição de Frequências' + '\n')
+print(tab)
+
+print('Distribuição de porcentagens' + '\n')
+print((tab / tab.sum())*100)
