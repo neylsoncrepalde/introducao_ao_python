e2cbe706a4dd8889781a248494c05b47dbdad944
Neylson Crepalde <neylsoncrepalde@gmail.com>
2018-05-02 17:32:44 -0300

    advances in data analysis

--- read_pnad2012.py
+++ read_pnad2012.py
@@ -8,6 +8,8 @@ Prof. Neylson
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 pnad = pd.read_csv('https://github.com/neylsoncrepalde/introducao_ao_python/blob/master/pes_2012.csv?raw=true')
 pnad.head()  # Verifica os primeiros casos
@@ -20,13 +22,19 @@ pnad.V0302.value_counts()
 pnad.V0404.value_counts() # Tem 'sem declaração mas tá ok...
 pnad.V8005.describe()
 pnad.V4803.value_counts() # Tem que transformar em numérica
-pnad.V4720.astype('float') # tem Sem declaração. Tem que transf em np.nan
+#pnad.V4720.astype('float') # tem Sem declaração. Tem que transf em np.nan
 pnad.V4729.describe()
 
 # Tira a média e a média ponderada pelo peso amostral da idade
 np.mean(pnad.V8005)
 np.average(pnad.V8005, weights = pnad.V4729)
 # Veja a diferença!
+
+"""
+Para outras estatísticas com peso amostral, utilizar a classe 
+statsmodels.stats.weightstats.DescrStatsW
+do pacote statsmodels
+"""
 
 # Transformando as rendas em float
 pnad.loc[pnad['V4718'] == 'Sem declaração', 'V4718'] = np.NaN
@@ -87,5 +95,72 @@ print('Distribuição de porcentagens' + '\n')
 print((tab / tab.sum())*100)
 
 # Fazendo uma tabela de freqência de duas variáveis
-tab2 = pd.crosstab(index=pnad.V0404, columns = pnad.V0302)
+tab2 = pd.crosstab(index=pnad.V0404, columns = pnad.V0302, margins = True)
 print(tab2)
+
+# Tabelas de frequência com porcentagens
+tab2_percent_row = pd.crosstab(index=pnad.V0404, columns = pnad.V0302, 
+                           margins = True, normalize = 'index') * 100
+print(tab2_percent_row)
+
+tab2_percent_col = pd.crosstab(index=pnad.V0404, columns = pnad.V0302, 
+                           margins = True, normalize = 'columns') * 100
+print(tab2_percent_col)
+
+tab2_percent_all = pd.crosstab(index=pnad.V0404, columns = pnad.V0302, 
+                           margins = True, normalize = 'all') * 100
+print(tab2_percent_all)
+
+
+### Algumas visualizações
+# Histograma
+sns.set(font_scale=1)
+fig, ax = plt.subplots()
+fig.set_size_inches(8, 4)
+sns.distplot(pnad.V4720.dropna(), kde = False)
+plt.title('Distribuição de renda - PNAD 2012')
+plt.xlabel('Renda')
+plt.ylabel('Frequência')
+plt.show()
+
+# Densidade
+fig, ax = plt.subplots()
+fig.set_size_inches(8, 6)
+sns.distplot(pnad.V4720.dropna(), hist = False)
+plt.title('Distribuição de renda - PNAD 2012')
+plt.xlabel('Renda')
+plt.ylabel('Gaussian Kernel Density Estimate')
+plt.show()
+
+# Cruzando renda e idade
+sns.jointplot(x = pnad.V4720, y = pnad.V8005, size = 8)
+plt.show()
+
+
+fig, ax = plt.subplots()
+fig.set_size_inches(8, 6)
+plt.scatter(x = pnad.V4720, y = pnad.V8005)
+plt.title('Gráfico de dispersão - Scatterplot', fontsize = 14)
+plt.xlabel("Renda")
+plt.ylabel("Idade")
+plt.show()
+
+
+### Plotando sexo e cor
+sns.set(color_codes = True)
+sns.countplot(x = pnad.V0302)
+plt.show()
+
+sns.countplot(y = pnad.V0302, color = 'c')
+plt.show()
+
+
+# Cruzando sexo e idade
+sns.boxplot(x = pnad.V0302, y = pnad.V8005)
+plt.show()
+
+sns.boxplot(y = pnad.V0302, x = pnad.V8005)
+plt.show()
+
+
+
